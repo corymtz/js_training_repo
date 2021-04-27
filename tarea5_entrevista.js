0a0692e8c6da92c3447f8ec3ec523ea74956cfb2
Cordelia Martinez <cmartinez@tacitknowledge.com>
2021-04-27 16:34:14 -0500

    adding homework js files

--- tarea5_entrevista.js
+++ tarea5_entrevista.js
@@ -1,6 +1,40 @@
-//contar el numero de vocales
 
 //arreglo de numeros, dividirlos de3 en 3 e imprimir el menor de cada grupo 
+var arreglo=[1,2,3,4,5,6,7,8]
+
+const divideArregloGuardaElMenor= (arreglo)=>{
+    var arrayNumerosMenores=[];
+    while(arreglo[0]){
+        arrayNumerosMenores.push(Math.min(...arreglo.splice(0,3)));
+    }
+    return arrayNumerosMenores;
+}
+
+console.log("Arreglo con numeros menores"+ divideArregloGuardaElMenor(arreglo));
 
 //descubrir si un numero es palindromo 
+const palindromo =(cadena)=>{
 
+var cadenaSinEspacios=cadena.replace(" ",""), 
+    x=cadenaSinEspacios.length-1,
+    esPalindrome=true;
+
+for(i=0;i<=x;){
+    if(cadenaSinEspacios.charAt(i)==cadenaSinEspacios.charAt(x)){
+        i=i+1;
+        x=x-1;
+    }else{
+        esPalindrome=false;
+        break;
+    }
+}
+return esPalindrome;
+
+
+}
+console.log("Es palidnromo?? :" + palindromo("race car"));
+
+//contar el numero de vocales
+var cadenaConVocales = "Ejercicio que cuenta vocales";
+var numeroVocales = cadenaConVocales.match(/[aeiou]/gi).length;
+console.log("Numero de vocales en:  "+cadenaConVocales+"= "+ numeroVocales);
